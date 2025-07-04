6ef76bd3c1d06d9f0f75eba355b83631a88ccf0a
Sana Sajjad <letsdreambig01@gmail.com>
2025-07-05 00:51:20 +0530

    learning javascript

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,6 +27,52 @@
   */
 
   // number to string me number whi rhega bs type change ho jayega to string
+
+  //************* operations *************
+
+  let val = 3
+  let negVal = -val
+  console.log(negVal);
+
+  let str1 = "hello"
+  let str2 = "sana"
+  let str3 = str1 + str2
+  console.log(str3);
+
+  console.log("1" + 2);  //12
+  console.log(1 + "2");  //12
+  console.log("1" + 2 + 2);  //122
+  console.log(1 + 2 + "2");  //32
+
+  // its done but should not be done
+  console.log(+true);  //1
+  console.log(+"");  //0
+
+  //prefix and postfix
+  x = 2
+  y = x++
+  console.log(x);  //3
+  console.log(y);  //2
+
+  a = 2
+  b = ++a
+  console.log(a);  //3
+  console.log(b);  //3
+
+  x = 2
+  y = x--
+  console.log(x);  //1
+  console.log(y);  //2
+
+  a = 2
+  b = --a
+  console.log(a);  //1
+  console.log(b);  //1
+  
+  
+  
+
+  
   
 
  
